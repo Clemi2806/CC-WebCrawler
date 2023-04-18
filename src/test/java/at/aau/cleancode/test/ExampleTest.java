276c15c97af3392a9a980c2851a1aad3868ec5c3
Clemi2806 <38780028+Clemi2806@users.noreply.github.com>
2023-04-18 10:21:50 +0200

    Set up project
    Set up project with Maven dependencies and .gitignore

---
+++ src/test/java/at/aau/cleancode/test/ExampleTest.java
@@ -0,0 +1,13 @@
+package at.aau.cleancode.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void additionTest(){
+        assertEquals(4, 2+2);
+    }
+}
