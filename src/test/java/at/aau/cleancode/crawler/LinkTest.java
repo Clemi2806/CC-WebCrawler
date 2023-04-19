059f54596d9d0fbd068769a21295bc7a3d985a55
jplanegger <joplanegger@edu.aau.at>
2023-04-19 23:20:25 +0200

    created testclasses

---
+++ src/test/java/at/aau/cleancode/crawler/LinkTest.java
@@ -0,0 +1,16 @@
+package at.aau.cleancode.crawler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkTest {
+
+    @Test
+    void isBroken() {
+    }
+
+    @Test
+    void getHref() {
+    }
+}
