1df35affc94d108b1a18650801167b306ecc3e25
jplanegger <joplanegger@edu.aau.at>
2023-04-19 21:07:17 +0200

    added Link object with URL checker

---
+++ src/main/java/at/aau/cleancode/crawler/Link.java
@@ -0,0 +1,36 @@
+package at.aau.cleancode.crawler;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Link {
+    private final String href;
+    private final boolean isBroken;
+
+    public Link(String href) {
+        this.href = href;
+        this.isBroken = !checkUrl(href);
+    }
+
+    public boolean isBroken() {
+        return isBroken;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    private boolean checkUrl(String href) {
+        try {
+            URL url = new URL(href);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            int responseCode = conn.getResponseCode();
+            return responseCode == 200;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
