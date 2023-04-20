74e443e15c61e834207b130d1bda97829eb30f65
jplanegger <joplanegger@edu.aau.at>
2023-04-20 10:45:49 +0200

    created tests for Link

--- src/test/java/at/aau/cleancode/crawler/LinkTest.java
+++ src/test/java/at/aau/cleancode/crawler/LinkTest.java
@@ -1,16 +1,28 @@
 package at.aau.cleancode.crawler;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkTest {
 
-    @Test
-    void isBroken() {
+    @ParameterizedTest
+    @MethodSource("linkSource")
+    void testGetters(String href, boolean expectedBroken) {
+        Link link = new Link(href);
+
+        assertEquals(href, link.getHref());
+        assertEquals(expectedBroken, link.isBroken());
     }
 
-    @Test
-    void getHref() {
+    private static Stream<Arguments> linkSource(){
+        return Stream.of(
+                Arguments.of("github", true),
+                Arguments.of("https://google.com", false),
+                Arguments.of("https://www.wikipedia.org", false));
     }
 }
